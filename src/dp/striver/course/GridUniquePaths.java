8fe815757f361bcda698d62b82496e2e4b3610c3
TamilselvanDevCodes <it.tamil2002@gmail.com>
2026-09-01 18:22:45 +0530

    DSA Learning (DP Striver Course Problems)
    Topic : DP on Grids — DP 8–13
    
    Problem : Grid Unique Paths with obstacles

--- src/dp/striver/course/GridUniquePaths.java
+++ src/dp/striver/course/GridUniquePaths.java
@@ -66,4 +66,35 @@ public class GridUniquePaths {
         }
         return prev[column - 1];
     }
+
+    private int getPathWithObstacleWithDPWithout2DArray(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[] prev = new int[n];
+        int[] curr = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                iterations++;
+
+                if (matrix[i][j] == -1) {
+                    curr[j] = 0;
+                }
+                else if (i == 0 && j == 0) {
+                    curr[j] = 1;
+                }
+                else {
+                    int up = prev[j];
+                    int left = j > 0 ? curr[j - 1] : 0;
+
+                    curr[j] = up + left;
+                }
+            }
+            prev = curr;
+            curr = new int[n];
+        }
+        return prev[n - 1];
+    }
 }
