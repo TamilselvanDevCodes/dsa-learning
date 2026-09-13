d6047af09e3816350dc9ae51cc12846c3a1ccc56
TamilselvanDevCodes <it.tamil2002@gmail.com>
2026-09-14 00:29:41 +0530

    DSA Learning (DP Striver Course Problems)
    Topic : Longest Increasing Subsequence — DP 41–47
    
    Problem : Print Longest Increasing Subsequence and code optimized

--- src/dp/striver/course/lis/LongestIncreasingSubsequence.java
+++ src/dp/striver/course/lis/LongestIncreasingSubsequence.java
@@ -1,6 +1,8 @@
 package dp.striver.course.lis;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class LongestIncreasingSubsequence {
     static long iterations=0;
@@ -8,7 +10,9 @@ public class LongestIncreasingSubsequence {
         LongestIncreasingSubsequence main =new LongestIncreasingSubsequence();
         int[]nums=new int[]{10,9,2,5,3,7,101,18};
         int res= main.lengthOfLIS(nums);
+        List<Integer>elements=main.getMaxLISElements(nums);
         System.out.println("res : "+res);
+        System.out.println("elements : "+elements);
         System.out.println("iterations : "+iterations);
     }
     public int lengthOfLIS(int[] nums) {
@@ -20,16 +24,57 @@ public class LongestIncreasingSubsequence {
 //        return getMaxLISCount(0,-1,nums);
 //        return getMaxLISCountWithDP(0,-1,dp,nums);
 //        return getMaxLISCountWithTabulation(nums);
-        return getMaxLISCountWith2Arrays(nums);
+//        return getMaxLISCountWith2Arrays(nums);
+        return getLISCountWith1Array(nums);
     }
-
-
+    private List<Integer> getMaxLISElements(int[]nums){
+        List<Integer> result=new ArrayList<>();
+        int n=nums.length;
+        int[]dp=new int[n];
+        int[]prevIndexCache=new int[n];
+        Arrays.fill(dp,1);
+        Arrays.fill(prevIndexCache,-1);
+        int resultIndex=0;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(nums[prev]<nums[i]){
+                    if((1+dp[prev])>dp[i]){
+                        dp[i]=1+dp[prev];
+                        prevIndexCache[i]=prev;
+                    }
+                }
+            }
+            if(dp[i]>=dp[resultIndex]){
+                resultIndex=i;
+            }
+        }
+        while (resultIndex!=-1){
+            result.add(nums[resultIndex]);
+            resultIndex=prevIndexCache[resultIndex];
+        }
+        return result.reversed();
+    }
+    private int getLISCountWith1Array(int[]nums){
+        int n=nums.length;
+        int[]dp=new int[n];
+        int resCount=1;
+        Arrays.fill(dp,1);
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(nums[prev]<nums[i]){
+                    dp[i]=Math.max(1+dp[prev],dp[i]);
+                }
+            }
+            resCount=Math.max(resCount,dp[i]);
+        }
+        return resCount;
+    }
     private int getMaxLISCountWith2Arrays(int[]nums){
         int n=nums.length;
         int[]next=new int[n+1];
         int[]curr=new int[n+1];
         for(int index=n-1;index>=0;index--){
-            for(int prevIndex=-1;prevIndex<n;prevIndex++){
+            for(int prevIndex=index-1;prevIndex>=-1;prevIndex--){
                 iterations++;
                 int notTake=next[prevIndex+1];
                 int take=0;
@@ -51,7 +96,7 @@ public class LongestIncreasingSubsequence {
         int n=nums.length;
         int[][]dp=new int[n+1][n+1];
         for(int index=n-1;index>=0;index--){
-            for(int prevIndex=-1;prevIndex<n;prevIndex++){
+            for(int prevIndex=index-1;prevIndex>=-1;prevIndex--){
                 iterations++;
                 int notTake=dp[index+1][prevIndex+1];
                 int take=0;
@@ -63,6 +108,7 @@ public class LongestIncreasingSubsequence {
         }
         return dp[0][0];
     }
+
 
 
 
